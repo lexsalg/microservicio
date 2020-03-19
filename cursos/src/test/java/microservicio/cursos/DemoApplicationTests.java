ec31e00c07e9909cf5341c8b1840c50241aac9be
Alexis Salgado Tacuri <xel.salg@gmail.com>
2020-03-19 01:07:05 -0500

    curso.

---
+++ cursos/src/test/java/microservicio/cursos/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package microservicio.cursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
